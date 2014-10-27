74d5f25c9eeb4feaf194eaf7500ae78788feec0e
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-10-27 12:27:13 +0100

    Add simple_form, clear links controller

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,5 +2,6 @@ class Subject < ActiveRecord::Base
   extend FriendlyId
   has_many :links
   friendly_id :name, use: :slugged
+  before_save { name.downcase! }
   validates :name, :slug, uniqueness: true
 end
--- app/views/links/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @link, :id, :url, :title, :description, :subject_id, :created_at, :updated_at
