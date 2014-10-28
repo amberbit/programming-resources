8b68d6f54d024eebd375e23ca077a8082d3a88ad
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-10-28 09:04:47 +0100

    Add regex for url validation, change errors css

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,9 @@
 class Link < ActiveRecord::Base
   belongs_to :subject
-  validates :title, :subject_name, :description, :url,  presence: true
+  VALID_URL_REGEX = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/
+  validates :url, format: { with: VALID_URL_REGEX },
+                        uniqueness: { case_sensitive: false }
+  validates :title, :subject_name, :description,  presence: true
   before_save {
     subject_name.downcase!
     subject = Subject.find_or_create_by(name: subject_name)
