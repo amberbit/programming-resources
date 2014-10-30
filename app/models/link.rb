a6a98de3438f40d6b918b5a52231acea0374a295
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-10-30 13:12:16 +0100

    Ensure that url is valid

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,15 @@
 class Link < ActiveRecord::Base
+  before_validation :add_protocol
   has_many :subjects
   has_many :links, through: :linkings
   VALID_URL_REGEX = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/
   validates :url, format: { with: VALID_URL_REGEX }
   validates :title, :description,  presence: true
+
+  protected
+    def add_protocol
+      unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+        self.url = "http://#{self.url}"
+      end
+    end
 end
