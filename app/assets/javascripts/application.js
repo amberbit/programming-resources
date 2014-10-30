1547272d4f993f06d1b9723ed6001690e0efd868
Aneta Bielska <aneta.bielska@yahoo.com>
2014-10-30 09:50:32 +0100

    added link to button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,3 @@
-//= require jquery
 //= require angular/angular
 //= require angular-bootstrap/ui-bootstrap-tpls
 //= require app
-
-$(document).ready(function($) {
-$(".add-form").hide();
-  $( ".to-form" ).click(function() {
-    $(".no-results").hide();
-    $(".add-form").show();
-  });
-});
