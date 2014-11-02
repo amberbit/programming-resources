3fbbc94e34c7df4b35e4befe9d9dc26ffc0db22d
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-11-02 18:13:10 +0100

    Add notice

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,6 @@ class VotesController < ApplicationController
     @linking = Linking.find(@link)
     score = params[:score] == "up" ? 1 : -1
     @linking.increment!(:scores, score)
-    redirect_to :back
+    redirect_to :back, notice: 'Thank you for voting!'
   end
 end
