a9f4d96aae6fc4d51770a8c97de5c55e8966e4ec
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-11-03 12:10:56 +0100

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'landing_pages#index'
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", as: :signout
+  match "/auth/:provider/callback" => "sessions#create", via:  :post
+  match "/signout" => "sessions#destroy", as: :signout, via:  :post
 
   resources :subjects do
     resources :links,  only: [:index, :new, :create]
