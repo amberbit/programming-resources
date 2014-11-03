a35c387c3ed15771f1a54c332527effc073ab557
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-11-03 09:43:11 +0100

    Add bootstrap-sprockets js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require angular/angular
-//= require angular-bootstrap/ui-bootstrap-tpls
 //= require jquery
 //= require jquery_ujs
-
+//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
+//= require angular-bootstrap/ui-bootstrap-tpls
 //= require app
+
