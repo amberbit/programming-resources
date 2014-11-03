ba6080103b4f95391f92e83031108a46f1957d18
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-11-03 12:04:20 +0100

    Add omni routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'landing_pages#index'
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", as: :signout
+
   resources :subjects do
     resources :links,  only: [:index, :new, :create]
   end
